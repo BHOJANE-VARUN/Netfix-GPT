4ad7134609175afcea59a1739ade3971919fd0cb
VarunBhojane <varunbhojane07@gmail.com>
2024-08-15 21:01:39 +0530

    Security changes

--- src/util/Links.js
+++ src/util/Links.js
@@ -1,13 +1,6 @@
 export const LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const PHOTO_URL =  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Fpremium-vector%2Fpeople-profile-graphic_24911-21373.jpg&tbnid=Y5vdNyNauoAmdM&vet=12ahUKEwjWt9is4OqHAxWG-jgGHXNuCr0QMygDegQIARBx..i&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fpeople-profile-graphic_2757319.htm&docid=rr8UIf2C1VmTRM&w=626&h=626&q=sample%20profile%20photo&ved=2ahUKEwjWt9is4OqHAxWG-jgGHXNuCr0QMygDegQIARBx";
 export const MOVIE_API = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
-export const OPTIONS = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NGY5MzVkYjc3MDFhMjlhOWE0YTg2NmU1NTVjYjM2ZCIsIm5iZiI6MTcyMzU5NzU2Ni45MDEzMDYsInN1YiI6IjY2YmMwMWE2OWVjOTYxZGMxZGMzM2VlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TlIollqNN7DOlQEzTr8QsNzk4zEqiUy88Sx5GDVtwVQ'
-  }
-};
 export const SEARCH_LEFT = 'https://api.themoviedb.org/3/search/multi?query=';
 export const SEARCH_RIGHT = '&include_adult=true&language=en-US&page=1';
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
